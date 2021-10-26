f4e288b8fd335f1285e83ab8779fb1963f3534b0
unknown <mpagarelski@gmail.com>
2021-10-26 00:39:28 -0400

    Completed Week 8 Intro to Powershell assignment

---
+++ Week08/homework/sendEmail.ps1
@@ -0,0 +1,18 @@
+﻿# Storyline: send an email.
+
+# Variable can have an underscore or any alphanumeric value
+
+# Body of the email
+$msg = "Hello there."
+
+Write-Host -BackgroundColor Red -ForegroundColor White $msg
+
+# Email From Address
+$email = "miranda.pagarelski@mymail.champlain.edu"
+
+
+# To address
+$toEmail = "deployer@csi-web"
+
+# Sending the email
+Send-MailMessage -From $email -To $toEmail -Subject "A Greeting" -Body $msg -SmtpServer 192.168.6.71

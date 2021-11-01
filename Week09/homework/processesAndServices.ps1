320e4fd5d4edd2e2019b80ecb721bb139ca7282f
unknown <mpagarelski@gmail.com>
2021-11-01 18:08:41 -0400

    Finished Week 9 Programming Assignment

---
+++ Week09/homework/processesAndServices.ps1
@@ -0,0 +1,12 @@
+﻿# Storyline: Using the Get-Process and Get-Service
+# Get-Process | Select-Object ProcessName, Path, ID | `
+# Export-Csv -Path "C:\Users\mpaga\Desktop\myProcesses.csv" -NoTypeInformation
+# Get-Process | Get-Member
+# Get-Service | Where { $_.Status  -eq "Stopped" }
+
+
+Get-Process | Select-Object ProcessName, Path, ID | `
+Export-Csv -Path "C:\Users\mpaga\FA21-SYS-320-02\Week09\homework\myProcesses.csv" -NoTypeInformation
+
+Get-Service | Where { $_.Status -eq "Running"} | `
+Export-Csv -Path "C:\Users\mpaga\FA21-SYS-320-02\Week09\homework\myRunningServices.csv" -NoTypeInformation
---
+++ Week09/homework/startAndStopCalculator.ps1
@@ -0,0 +1,7 @@
+﻿# Write a program that can start and stop the Windows Calculator only using Powershell 
+# and using only the process name for the Windows calculator (to start and stop it)
+
+
+Start-Process win32calc 
+Start-Sleep 5
+Stop-Process -Name win32calc
